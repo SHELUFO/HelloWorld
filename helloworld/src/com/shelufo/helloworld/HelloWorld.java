b5979e84b74a74533a36fa2ddef59fc8968e9462
SHELUFO <105379386+SHELUFO@users.noreply.github.com>
2023-02-10 23:05:59 +0800

    尝试第二次更新

--- helloworld/src/com/shelufo/helloworld/HelloWorld.java
+++ helloworld/src/com/shelufo/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package com.shelufo.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("第一次尝试更新");
+        System.out.println("第二次尝试更新");
     }
 }
